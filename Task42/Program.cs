c0c2c0b39e458690255aa5e3ed22efa1dc5e3499
Pavel <grigorov_ps@mail.ru>
2023-10-27 15:01:56 +0300

    Создали проект Task42 (преобразовывает десятичное число в двоичное)

---
+++ Task42/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 42: 
+// Напишите программу, которая 
+// будет преобразовывать десятичное число в двоичное.
+// 46 -> 101110
+// 13 -> 1101
+// 2 -> 10
+
+int ConvertDecemalToBinary(int num)
+{
+    int res = 0;
+    int mult = 1; // Добавляем множитель для увеличения результата каждой ИТЕРАЦИИ
+    while (num > 0)
+    {
+        res = res + num % 2 * mult; 
+        num = num / 2;
+        mult = mult * 10;
+    }
+    return res;
+
+}
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = ConvertDecemalToBinary(number);
+Console.WriteLine(result);
